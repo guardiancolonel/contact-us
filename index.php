5b5ebc47b7363d2eb313411a026dc19a594e54da
Srijidh <srijidhmadhavan@quinconx.com>
2016-12-14 23:52:09 +0530

    Add formvalidation code to main js

--- index.php
+++ index.php
@@ -81,49 +81,5 @@
         <script src="js/plugins.js"></script>
         <script src="js/main.js"></script>
 
-        <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
-        <script>
-            $('#contact_form').on('submit', function(e) {
-            	e.preventDefault();
-              var validForm = true;
-
-              // Form Validation
-              
-              $(this).find('input').each(function(key, value) {
-                  if ( $(value).val().length === 0 ) {
-                    console.log($(value).attr('name') + " Can't be empty");
-                    validForm = false;
-                  }
-
-                  if ( $(value).attr('name') === 'email' ) {
-                    if ( ! validateEmail( $(value).val() ) ) {
-                      console.log(" Not a valid email");
-                      validForm = false;
-                    }
-                  }
-              });
-
-              function validateEmail(email) {
-                  var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-                  return re.test(email);
-              }
-            	
-            	// Server Messages
-              if ( validForm === true) {
-                var formData = $(this).serialize();
-                  $.ajax({
-                  url: "contact_us.php",
-                  type: "POST",
-                  data: formData,
-                  datatType: "json"
-                }).done(function( data ) {
-                  console.log( data );
-                }).fail(function( data ) {
-                  console.log( data );
-                });
-              }
-            	
-            });
-        </script>
     </body>
 </html>
