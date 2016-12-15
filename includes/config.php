61c8f14b17e6fc0b6db0afba90fee4393dce7907
Srijidh <srijidhmadhavan@quinconx.com>
2016-12-15 20:35:19 +0530

    COnfiguration file added

--- includes/config.php
+++ includes/config.php
@@ -5,3 +5,10 @@ error_reporting(E_ALL);
 
 define("TO_NAME", 'Srijidh');
 define("TO_EMAIL", 'srijidhmadhavan@gmail.com');
+
+$fields = array(
+	'name' => 'name',
+	'email' => 'email',
+	'suject' => 'subject'
+
+	);
